8780fd9ba94ea200e083632e0e16577d91609635
MickiRhen <mickirhen@gmail.com>
2021-05-30 13:52:07 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='whatif',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='A walk through of the aap what_if_4_project_packaging',
+    author='Michelle Rhen',
+    license='',
+)
